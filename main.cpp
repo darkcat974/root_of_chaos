6323f55004d3ef32cebc3b0324bead9a48a96eaf
dred974 <guillaume.lallemand@epitech.eu>
2023-02-04 22:21:59 +0400

    add buttons exit and images

--- main.cpp
+++ main.cpp
@@ -9,6 +9,13 @@
 #include <SFML/System.hpp>
 #include "./include/root.hpp"
 
+enum game click_exit1(enum game play)
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        return MENU;
+    return play;
+}
+
 enum game click_menu(sf::Sprite sprite, sf::Texture texture, enum game play)
 {
     sf::Texture texture2;
@@ -39,9 +46,21 @@ enum game click_credits(sf::Sprite sprite, enum game play)
     return play;
 }
 
+enum game click_help(sf::Sprite sprite, enum game play)
+{
+    sf::Vector2i move = sf::Mouse::getPosition();
+
+    sf::FloatRect rect = sprite.getGlobalBounds();
+    if (rect.contains(move.x, move.y)) {
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            return HELP;
+        }
+    }
+    return play;
+}
+
 void click_quit(sf::Sprite sprite)
 {
-    sf::Texture texture2;
     sf::Vector2i move = sf::Mouse::getPosition();
 
     sf::FloatRect rect = sprite.getGlobalBounds();
@@ -77,6 +96,14 @@ int main(void)
     credits1.setPosition(sf::Vector2f(100, 450));
     credits1.setScale(sf::Vector2f(1.5, 1));
 
+    sf::Texture text;
+    text.loadFromFile("images/credits.png");
+
+    sf::Sprite credits;
+    credits.setTexture(text);
+    credits.setPosition(sf::Vector2f(0, 0));
+    credits.setScale(sf::Vector2f(1, 1));
+
     sf::Texture texture3;
     texture3.loadFromFile("images/QUIT1.png");
 
@@ -93,6 +120,22 @@ int main(void)
     fond.setPosition(sf::Vector2f(0, 0));
     fond.setScale(sf::Vector2f(1, 1));
 
+    sf::Texture help1;
+    help1.loadFromFile("images/help.png");
+
+    sf::Sprite h;
+    h.setTexture(help1);
+    h.setPosition(sf::Vector2f(1800, 970));
+    h.setScale(sf::Vector2f(1, 1));
+
+    sf::Texture help2;
+    help2.loadFromFile("images/HELP.png");
+
+    sf::Sprite h2;
+    h2.setTexture(help2);
+    h2.setPosition(sf::Vector2f(0, 0));
+    h2.setScale(sf::Vector2f(1, 1));
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -104,6 +147,8 @@ int main(void)
         window.clear();
         if (play == MENU) {
             window.draw(fond);
+            play = click_help(h, play);
+            window.draw(h);
             play = click_menu(play1, texture1, play);
             window.draw(play1);
             play = click_credits(credits1, play);
@@ -111,10 +156,16 @@ int main(void)
             window.draw(qui1);
         }
         if (play == GAME) {
+            play = click_exit1(play);
             window.clear(sf::Color::Red);
         }
         if (play == CREDITS) {
-            window.clear(sf::Color::Blue);
+            play = click_exit1(play);
+            window.draw(credits);
+        }
+        if (play == HELP) {
+            play = click_exit1(play);
+            window.draw(h2);
         }
         click_quit(qui1);
         window.display();
