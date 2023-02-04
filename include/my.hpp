ebc70d86ae65eaee8d212d7f436716af0153ac71
NACAOUELEROMAIN <romain.nacaouele@epitech.eu>
2023-02-04 22:27:21 +0400

    fonc for sprite

--- include/my.hpp
+++ include/my.hpp
@@ -3,6 +3,9 @@
 #include <SFML/Audio.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
+#include <cstdlib>
+#include <iostream>
+#define V2F(x, y) sf::Vector2f(x, y)
 
 class Options
 {
@@ -11,14 +14,30 @@ public:
     Options();
     ~Options();
     sf::RenderWindow &screen(sf::VideoMode);
-    sf::Sprite &sprite_gen(std::string path);
     void game_loop();
 
 private:
     sf::RenderWindow window;
-    sf::Texture background;
-    sf::Sprite entity;
     unsigned int height;
     unsigned int width;
     unsigned int pix;
+};
+
+class Sprite
+{
+ 
+public:
+    Sprite();
+    ~Sprite();
+    sf::Sprite &sprite_gen(std::string path, sf::Vector2f, sf::Vector2f);
+    sf::Sprite &sprite_gen2(std::string path, sf::Vector2f, sf::Vector2f);
+    void game_loop();
+
+    sf::Texture background;
+    sf::Sprite entity;
+    sf::Texture play;
+    sf::Sprite entity2;
+
+private:
+    sf::RenderWindow window;
 };
--- main.cpp
+++ main.cpp
@@ -12,6 +12,14 @@ Options::~Options()
 {
 }
 
+Sprite::Sprite()
+{
+}
+
+Sprite::~Sprite()
+{
+}
+
 sf::RenderWindow &Options::screen(sf::VideoMode videomode)
 {
     this->height = videomode.height;
@@ -21,19 +29,39 @@ sf::RenderWindow &Options::screen(sf::VideoMode videomode)
     return window;
 }
 
-sf::Sprite &Options::sprite_gen(std::string path)
+sf::Sprite &Sprite::sprite_gen(std::string path, sf::Vector2f position, sf::Vector2f size)
 {
     if(!background.loadFromFile(path))
     {
         std::cout << "Erreur de chargement" <<  std::endl;
     }
     background.setSmooth(true);
+
+    this->entity.setPosition(position);
+    this->entity.setScale(size);
     this->entity.setTexture(background);
     return entity;
 }
 
+sf::Sprite &Sprite::sprite_gen2(std::string path, sf::Vector2f position, sf::Vector2f size)
+{
+    if(!play.loadFromFile(path))
+    {
+        std::cout << "Erreur de chargement" <<  std::endl;
+    }
+    background.setSmooth(true);
+
+    this->entity2.setPosition(position);
+    this->entity2.setScale(size);
+    this->entity2.setTexture(play);
+    return entity2;
+}
+
 void  Options::game_loop()
 {
+    Sprite for_sprite;
+    for_sprite.sprite_gen2("asset/play_blue.png", V2F(0, 0), V2F(0.5, 0.5));
+    for_sprite.sprite_gen("asset/last_choise_map1.png", V2F(0, -20), V2F(2.2, 2));
     while (window.isOpen())
     {
         sf::Event event;
@@ -44,7 +72,8 @@ void  Options::game_loop()
         }
 
         window.clear();
-        window.draw(entity);
+        window.draw(for_sprite.entity);
+        window.draw(for_sprite.entity2);
         window.display();
     }
 }
@@ -52,8 +81,8 @@ void  Options::game_loop()
 int main()
 {
     Options for_screen;
+    Sprite for_sprite;
     for_screen.screen(sf::VideoMode::getDesktopMode());
-    for_screen.sprite_gen("asset/menu.png");
     for_screen.game_loop();
     return 0;
 }
